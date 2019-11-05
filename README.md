4f108c0a2cf854ffc3d0df9b4bf4df2caf8bc475
Oleg Kozlov <h1karo@outlook.com>
2019-11-06 01:32:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ShareControl
+This is the official repository of ShareControl Minecraft server plugin. Repository have only 3.0+ plugin versions.
+
+ShareControl supports these server cores:
+1. Spigot
+2. PaperSpigot
+3. Bukkit
---
+++ src/main/java/ru/h1karo/sharecontrol/ShareControl.java
@@ -0,0 +1,38 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2019 ShareControl
+ * @author h1karo <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShareControl extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
