bfbc2fbeb18dda14b3f7e89b25b4c45793ca5ab3
Oleg Kozlov <h1karo@outlook.com>
2020-07-10 23:34:39 +0300

    Implement yaml stream loader

---
+++ src/main/java/ru/h1karo/sharecontrol/i18n/loader/InputStreamLoader.kt
@@ -0,0 +1,41 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2020 ShareControl
+ * @author Oleg Kozlov <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol.i18n.loader
+
+import ru.h1karo.sharecontrol.i18n.Locale
+import ru.h1karo.sharecontrol.i18n.MessageCatalogue
+import java.io.*
+
+abstract class InputStreamLoader : MapLoader() {
+    override fun load(resource: Any, locale: Locale): MessageCatalogue {
+        if (resource !is InputStream) {
+            throw IllegalArgumentException("%s can load messages only from input stream.".format(this::class.java))
+        }
+
+        val reader = InputStreamReader(resource)
+        val messages = this.loadFrom(reader)
+        return super.load(messages, locale)
+    }
+
+    abstract fun loadFrom(reader: Reader): Map<String, Any>
+}
