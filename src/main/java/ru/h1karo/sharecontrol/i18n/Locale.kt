43c9bc17ae4f3e03cd99fd58df56bc2bb88b6352
Oleg Kozlov <h1karo@outlook.com>
2020-09-27 17:53:51 +0300

    Override locale hashCode and equals methods

--- src/main/java/ru/h1karo/sharecontrol/i18n/Locale.kt
+++ src/main/java/ru/h1karo/sharecontrol/i18n/Locale.kt
@@ -28,4 +28,22 @@ data class Locale(val abbr: String) : ParameterValue<String> {
     lateinit var name: String
 
     override fun getValue(): String = this.abbr
+
+    override fun hashCode(): Int {
+        return this.abbr.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) {
+            return true
+        }
+
+        if (javaClass != other?.javaClass) {
+            return false
+        }
+
+        other as Locale
+
+        return abbr == other.abbr
+    }
 }
