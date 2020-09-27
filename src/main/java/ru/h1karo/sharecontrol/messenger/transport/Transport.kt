3c8f3ff83d7fec1a45d8f36b0029878dacafa00c
Oleg Kozlov <h1karo@outlook.com>
2020-09-27 23:48:53 +0300

    Implement delegating messenger and console transport

---
+++ src/main/java/ru/h1karo/sharecontrol/messenger/transport/Transport.kt
@@ -0,0 +1,28 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2020 ShareControl
+ * @author Oleg Kozlov <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol.messenger.transport
+
+interface Transport {
+    fun send(recipient: Any, message: String)
+    fun supports(recipient: Any, message: String): Boolean
+}
