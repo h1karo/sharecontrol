2e0330168338b9b192ddc0d4736be90816e69aa2
Oleg Kozlov <h1karo@outlook.com>
2020-10-10 21:51:29 +0300

    Implement command class template

---
+++ src/main/java/ru/h1karo/sharecontrol/command/Command.kt
@@ -0,0 +1,41 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2020 ShareControl
+ * @author Oleg Kozlov <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol.command
+
+abstract class Command(
+    name: String,
+    aliases: Set<String> = emptySet(),
+    arguments: Set<Argument> = emptySet(),
+    options: Set<Option> = emptySet(),
+) {
+    private val name = name
+        get() = field
+    private val aliases = aliases
+        get() = field
+    private val options = options
+        get() = field
+    private val arguments = arguments
+        get() = field
+
+    abstract fun run()
+}
