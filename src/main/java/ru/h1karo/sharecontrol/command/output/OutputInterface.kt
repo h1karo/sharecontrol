32c6b5c54deb76fffc7afcdf64801866d5a24f11
Oleg Kozlov <h1karo@outlook.com>
2020-10-15 20:46:41 +0300

    Add default value for parameters in output interface

--- src/main/java/ru/h1karo/sharecontrol/command/output/OutputInterface.kt
+++ src/main/java/ru/h1karo/sharecontrol/command/output/OutputInterface.kt
@@ -23,5 +23,5 @@
 package ru.h1karo.sharecontrol.command.output
 
 interface OutputInterface {
-    fun write(message: String, parameters: Set<String>)
+    fun write(message: String, parameters: Set<String> = setOf())
 }
