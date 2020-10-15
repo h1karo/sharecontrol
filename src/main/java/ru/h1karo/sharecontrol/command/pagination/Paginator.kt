d198e8797d8ecc5042dd11b0524dabfcdd8695e5
Oleg Kozlov <h1karo@outlook.com>
2020-10-15 20:42:22 +0300

    Implement paginator1

---
+++ src/main/java/ru/h1karo/sharecontrol/command/pagination/Paginator.kt
@@ -0,0 +1,42 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2020 ShareControl
+ * @author Oleg Kozlov <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol.command.pagination
+
+import ru.h1karo.sharecontrol.command.exception.PageNumberOutOfRangeException
+
+class Paginator : PaginatorInterface {
+    override fun paginate(content: List<String>, page: Int, limit: Int): PaginationInterface {
+        if (page <= 0 || limit <= 0) {
+            throw IllegalArgumentException("The page and limit parameters must be positive non-zero integers.")
+        }
+
+        val pageIndex = page - 1
+        val pages = content.chunked(limit)
+        val maxPage = pages.size
+        if (maxPage <= pageIndex) {
+            throw PageNumberOutOfRangeException(page, maxPage)
+        }
+
+        return Pagination(pages[pageIndex], page, limit, maxPage)
+    }
+}
