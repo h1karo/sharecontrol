f61a86c9f55516c9057d1a89d7f4ceb60b21280d
Oleg Kozlov <h1karo@outlook.com>
2021-01-26 01:15:53 +0300

    Implement about command

---
+++ src/main/java/ru/h1karo/sharecontrol/command/AboutCommand.kt
@@ -0,0 +1,52 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2020 ShareControl
+ * @author Oleg Kozlov <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol.command
+
+import com.google.inject.Inject
+import org.bukkit.plugin.Plugin
+import ru.h1karo.sharecontrol.command.input.InputInterface
+import ru.h1karo.sharecontrol.command.output.OutputInterface
+import ru.h1karo.sharecontrol.command.style.OutputStyle
+
+class AboutCommand @Inject constructor(
+    private val plugin: Plugin,
+    override val parent: ShareControlCommand
+) : Command() {
+    override val name: String = NAME
+
+    override fun execute(input: InputInterface, output: OutputInterface): Boolean {
+        val style = OutputStyle(output)
+        val description = plugin.description
+
+        style.write("about.title")
+        style.write("about.version", listOf(description.version))
+        style.write("about.author", listOf(description.authors.first()))
+        style.write("about.website", listOf(description.website ?: "n/a"))
+
+        return true
+    }
+
+    companion object {
+        private const val NAME = "about"
+    }
+}
