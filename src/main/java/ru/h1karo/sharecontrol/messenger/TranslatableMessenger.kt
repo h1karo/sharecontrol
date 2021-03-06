49e8544091418e5bf385c253cb889d6fb1b1997f
Oleg Kozlov <h1karo@outlook.com>
2021-03-07 02:39:00 +0300

    Allow to translate inner messages

--- src/main/java/ru/h1karo/sharecontrol/messenger/TranslatableMessenger.kt
+++ src/main/java/ru/h1karo/sharecontrol/messenger/TranslatableMessenger.kt
@@ -25,6 +25,7 @@ package ru.h1karo.sharecontrol.messenger
 import com.google.inject.Inject
 import ru.h1karo.sharecontrol.i18n.TranslatorInterface
 import ru.h1karo.sharecontrol.i18n.exception.I18nException
+import java.text.MessageFormat
 
 class TranslatableMessenger @Inject constructor(
     private val messenger: Messenger,
@@ -32,10 +33,32 @@ class TranslatableMessenger @Inject constructor(
 ) : Messenger {
     override fun send(recipient: Any, message: String, parameters: Collection<Any>) {
         try {
-            val translated = this.translator.trans(message, parameters)
+            val translated = this.translate(message, parameters)
             this.messenger.send(recipient, translated)
         } catch (e: I18nException) {
             this.messenger.send(recipient, message, parameters)
         }
+    }
+
+    private fun translate(message: String, parameters: Collection<Any>): String {
+        return try {
+            this.translator.trans(message, parameters)
+        } catch (e: I18nException) {
+            this.findTranslationKeys(message)
+                .map { key -> Pair(MessageFormat.format(KEY_FORMAT, key), this.translator.trans(key)) }
+                .fold(message, { translated, pair -> translated.replace(pair.first, pair.second) })
+        }
+    }
+
+    private fun findTranslationKeys(message: String): Collection<String> {
+        return KEY_PATTERN
+            .findAll(message)
+            .map { it.groupValues[1] }
+            .toSet()
+    }
+
+    companion object {
+        private const val KEY_FORMAT = "\$'{'{0}'}'"
+        private val KEY_PATTERN = Regex("\\\$\\{([a-z]+(\\.[a-z]+)?)}")
     }
 }
