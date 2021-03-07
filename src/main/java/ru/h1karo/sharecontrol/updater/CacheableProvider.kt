365606f9b67bb23e1cd98f365c1ea6a929d88545
Oleg Kozlov <h1karo@outlook.com>
2021-03-08 02:45:09 +0300

    Implement cacheable version provider

---
+++ src/main/java/ru/h1karo/sharecontrol/updater/CacheableProvider.kt
@@ -0,0 +1,35 @@
+/*
+ * This file is a part of ShareControl.
+ *
+ * ShareControl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ShareControl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ShareControl. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @copyright Copyright (c) 2020 ShareControl
+ * @author Oleg Kozlov <h1karo@outlook.com>
+ * @license GNU General Public License v3.0
+ * @link https://github.com/h1karo/sharecontrol
+ */
+
+package ru.h1karo.sharecontrol.updater
+
+class CacheableProvider(private val provider: VersionProvider) : VersionProvider {
+    private var version: Version? = null
+
+    override fun find(): Version? {
+        if (this.version == null) {
+            this.version = this.provider.find()
+        }
+
+        return this.version
+    }
+}
